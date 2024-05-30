322fdc7459b26df5b2cd9f242301c564939916d7
Quindarts <quang82thcspb@gmail.com>
2024-05-30 13:55:40 +0700

    fix: logic lecturer import excel

--- src/middleware/lecturer.middleware.js
+++ src/middleware/lecturer.middleware.js
@@ -12,11 +12,13 @@ exports.protectLecturer = async (req, res, next) => {
         if (!token) {
             return Error.sendUnauthenticated(res);
         }
+        const decoded = await verifyAccessToken(token);
 
-        const decoded = verifyAccessToken(token);
-        const lecturer = await Lecturer.findByPk(decoded.id);
-        console.log("🚀 ~ exports.protectLecturer= ~ lecturer:", lecturer)
+        if (decoded.expired) {
+            return Error.sendUnauthenticated(res);
+        }
 
+        const lecturer = await Lecturer.findByPk(decoded.payload.id);
         if (!lecturer) {
             return Error.sendUnauthenticated(res);
         }
