5a7afa07f13736b0f6ba9750a6536fb43356e576
hoangnguyendev19 <hoangngocnguyen2021@gmail.com>
2024-06-28 21:57:59 +0700

    Update transcript controller and route

--- src/routes/transcript.route.js
+++ src/routes/transcript.route.js
@@ -10,7 +10,6 @@ const {
 } = require('../controllers/transcript.controller');
 const { protectLecturer } = require('../middleware/lecturer.middleware');
 const { protectStudent } = require('../middleware/student.middleware');
-const { isExistTranscript } = require('../middleware/transcript.middleware');
 
 const router = express.Router();
 
@@ -18,7 +17,7 @@ router.get(APP_ROUTER.INDEX, getTranscriptByTypeEvaluation);
 
 router.get(APP_ROUTER.TRANSCRIPT_BY_SUMMARY, protectStudent, getTranscriptSummary);
 
-router.post(APP_ROUTER.INDEX, protectLecturer, isExistTranscript, createTranscript);
+router.post(APP_ROUTER.INDEX, protectLecturer, createTranscript);
 
 router.put(APP_ROUTER.ID, protectLecturer, updateTranscript);
 
