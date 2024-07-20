c3278e935907f7d438eadb817c6b652106b3306e
hoangnguyendev19 <hoangngocnguyen2021@gmail.com>
2024-07-20 13:34:25 +0700

    Update updateTranscriptList

--- src/controllers/transcript.controller.js
+++ src/controllers/transcript.controller.js
@@ -132,127 +132,35 @@ exports.getTranscriptByGroupStudent = async (req, res) => {
 };
 
 exports.getTranscriptSummary = async (req, res) => {
-    // try {
-    //     const { termId } = req.query;
-
-    //     const transcripts = await sequelize.query(
-    //         `SELECT s.id, e.type, (sum(t.score) / sum(e.score_max)) * 10 as avgScore
-    //         FROM transcripts t
-    //         INNER JOIN evaluations e ON t.evaluation_id = e.id
-    //         INNER JOIN student_terms st ON t.student_term_id = st.id
-    //         INNER JOIN students s ON st.student_id = s.id
-    //         WHERE st.term_id = :termId AND s.id = :studentId
-    //         GROUP BY s.id, e.type`,
-    //         {
-    //             replacements: {
-    //                 termId,
-    //                 studentId: req.user.id,
-    //             },
-    //             type: sequelize.QueryTypes.SELECT,
-    //         },
-    //     );
-
-    //     // I want to fix the avgScore to 2 decimal places
-    //     transcripts.forEach((transcript) => {
-    //         transcript.avgScore = Number(transcript.avgScore.toFixed(2));
-    //     });
-
-    //     res.status(HTTP_STATUS.OK).json({
-    //         success: true,
-    //         message: 'Get Success',
-    //         transcripts,
-    //     });
-    // } catch (error) {
-    //     console.log(error);
-    //     Error.sendError(res, error);
-    // }
-
     try {
         const { termId } = req.query;
 
-        const studentTerm = await StudentTerm.findOne({
-            where: {
-                term_id: termId,
-                student_id: req.user.id,
+        const transcripts = await sequelize.query(
+            `SELECT s.id, e.type, (sum(t.score) / sum(e.score_max)) * 10 as avgScore
+            FROM transcripts t
+            INNER JOIN evaluations e ON t.evaluation_id = e.id
+            INNER JOIN student_terms st ON t.student_term_id = st.id
+            INNER JOIN students s ON st.student_id = s.id
+            WHERE st.term_id = :termId AND s.id = :studentId
+            GROUP BY s.id, e.type`,
+            {
+                replacements: {
+                    termId,
+                    studentId: req.user.id,
+                },
+                type: sequelize.QueryTypes.SELECT,
             },
-        });
-
-        if (!studentTerm) {
-            return Error.sendNotFound(res, 'Student term not found');
-        }
-
-        const achievements = await Achievement.findAll({
-            where: {
-                student_term_id: studentTerm.id,
-            },
-            attributes: ['id', 'bonusScore'],
-        });
-
-        const totalBonusScore = achievements.reduce(
-            (total, achievement) => total + achievement.bonusScore,
-            0,
         );
 
-        const transcripts = await Transcript.findAll({
-            where: {
-                student_term_id: studentTerm.id,
-            },
-            attributes: ['id', 'score'],
-            include: [
-                {
-                    model: Evaluation,
-                    attributes: ['type'],
-                    as: 'evaluation',
-                },
-            ],
+        // I want to fix the avgScore to 2 decimal places
+        transcripts.forEach((transcript) => {
+            transcript.avgScore = Number(transcript.avgScore.toFixed(2));
         });
-
-        // I want to calculate the average score of each type of evaluation
-        const evaluationTypes = [
-            ...new Set(transcripts.map((transcript) => transcript.evaluation.type)),
-        ];
-        const evaluationSummary = evaluationTypes.map((type) => {
-            const evaluationTranscripts = transcripts.filter(
-                (transcript) => transcript.evaluation.type === type,
-            );
-            const totalScore = evaluationTranscripts.reduce(
-                (total, transcript) => total + transcript.score,
-                0,
-            );
-
-            let averageScore = 0;
-
-            if (totalScore !== 0) {
-                averageScore = (totalScore / evaluationTranscripts.length).toFixed(2);
-            }
-
-            return {
-                type,
-                averageScore,
-            };
-        });
-
-        const advisor = evaluationSummary.find((evaluation) => evaluation.type === 'ADVISOR');
-        const report = evaluationSummary.find((evaluation) => evaluation.type === 'SESSION_HOST');
-        const reviewer = evaluationSummary.find((evaluation) => evaluation.type === 'REVIEWER');
-
-        // I want to calculate the total average score of all evaluations
-        const totalScore = transcripts.reduce((total, transcript) => total + transcript.score, 0);
-        let totalAverageScore = 0;
-        if (totalScore !== 0) {
-            totalAverageScore = (totalScore / transcripts.length).toFixed(2);
-        }
 
         res.status(HTTP_STATUS.OK).json({
             success: true,
             message: 'Get Success',
-            transcript: {
-                advisorScore: advisor ? Number(advisor.averageScore) : 0,
-                reportScore: report ? Number(report.averageScore) : 0,
-                reviewerScore: reviewer ? Number(reviewer.averageScore) : 0,
-                totalBonusScore: totalBonusScore ? Number(totalBonusScore) : 0,
-                totalAverageScore: totalAverageScore ? Number(totalAverageScore) : 0,
-            },
+            transcripts,
         });
     } catch (error) {
         console.log(error);
@@ -365,91 +273,12 @@ exports.createTranscriptList = async (req, res) => {
     }
 };
 
-exports.createTranscript = async (req, res) => {
-    try {
-        const { termId, studentId, score, evaluationId } = req.body;
-        const lecturerTerm = await LecturerTerm.findOne({
-            where: {
-                term_id: termId,
-                lecturer_id: req.user.id,
-            },
-        });
-
-        if (!lecturerTerm) {
-            return Error.sendNotFound(res, 'Giảng viên không tồn tại trong học kỳ!');
-        }
-
-        const studentTerm = await StudentTerm.findOne({
-            where: {
-                term_id: termId,
-                student_id: studentId,
-            },
-        });
-
-        if (!studentTerm) {
-            return Error.sendNotFound(res, 'Sinh viên không tồn tại trong học kỳ!');
-        }
-
-        const evaluation = await Evaluation.findByPk(evaluationId);
-
-        if (score > evaluation.scoreMax) {
-            return Error.sendWarning(res, 'Điểm không được lớn hơn điểm tối đa của đánh giá!');
-        }
-
-        const transcript = await Transcript.create({
-            lecturer_term_id: lecturerTerm.id,
-            student_term_id: studentTerm.id,
-            evaluation_id: evaluationId,
-            score,
-        });
-
-        res.status(HTTP_STATUS.CREATED).json({
-            success: true,
-            message: 'Create Success',
-            transcript,
-        });
-    } catch (error) {
-        console.log(error);
-        Error.sendError(res, error);
-    }
-};
-
-exports.updateTranscript = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { score } = req.body;
-
-        const transcript = await Transcript.findByPk(id);
-
-        if (!transcript) {
-            return Error.sendNotFound(res, 'Bảng điểm không tồn tại!');
-        }
-
-        const evaluation = await Evaluation.findByPk(transcript.evaluation_id);
-        if (score > evaluation.scoreMax) {
-            return Error.sendWarning(res, 'Điểm không được lớn hơn điểm tối đa của đánh giá!');
-        }
-
-        transcript.score = score;
-
-        await transcript.save();
-
-        res.status(HTTP_STATUS.OK).json({
-            success: true,
-            message: 'Update Success',
-        });
-    } catch (error) {
-        console.log(error);
-        Error.sendError(res, error);
-    }
-};
-
 exports.updateTranscriptList = async (req, res) => {
     try {
         const { transcripts } = req.body;
 
-        const transcriptPromises = transcripts.map(async (transcript) => {
-            const { id, score } = transcript;
+        const transcriptPromises = transcripts.map(async (trans) => {
+            const { id, score } = trans;
 
             const transcript = await Transcript.findByPk(id);
 
