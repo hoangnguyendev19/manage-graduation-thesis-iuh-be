0a332b47b26ac5418f30399380a37ee3a324dcfe
hoangnguyendev19 <hoangngocnguyen2021@gmail.com>
2024-11-15 13:35:55 +0700

    Add plagiarism and AI content check routes, implement corresponding controllers for file validation and processing

--- src/routes/check.route.js
+++ src/routes/check.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { APP_ROUTER } = require('../constants/router');
-const { checkPlagiarism } = require('../controllers/check.controller');
+const { checkPlagiarism, checkAiContent } = require('../controllers/check.controller');
 const upload = require('../configs/uploadConfig');
 
 const router = express.Router();
 
-router.post(APP_ROUTER.INDEX, upload.single('file'), checkPlagiarism);
+router.post(APP_ROUTER.CHECK_PLAGIARISM, upload.single('file'), checkPlagiarism);
+router.post(APP_ROUTER.CHECK_AI_CONTENT, upload.single('file'), checkAiContent);
 
 module.exports = router;
